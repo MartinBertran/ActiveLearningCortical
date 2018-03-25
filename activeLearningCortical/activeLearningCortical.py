23e8f9649524d16d9119e7d5473cdcb65878647a
Martinb <martin.a.bertran@gmail.com>
2018-03-25 13:10:09 -0400

    approximate PVal sieve added

--- activeLearningCortical/activeLearningCortical.py
+++ activeLearningCortical/activeLearningCortical.py
@@ -22,7 +22,7 @@ class ClassModel():
     ref to paper
     '''
 
-    def __init__(self, D_c_l, D_c_u, D_s_l, D_s_u, k,kappa, X, I, gamma, nu, n_splits, sensitivity_th=0.1, beta=1/4, verbose= False, logfile=None, checkpoint=None):
+    def __init__(self, D_c_l, D_c_u, D_s_l, D_s_u, k,kappa, X, I, gamma=1e-3, nu=0.7, n_splits=0.7, beta=1/4, sensitivity_th=0.1, verbose= False, logfile=None, checkpoint=None):
 
         self.verbose = verbose
         self.logfile = logfile
